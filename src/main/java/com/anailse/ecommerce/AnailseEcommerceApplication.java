0703346d72e16787237016574aca4cc0aa51b924
CLAROGT\brayner.navarro <brayner.navarro@claro.com.gt>
2024-08-26 16:29:45 -0600

    inicio de proyecto ecommerce de anailse

---
+++ src/main/java/com/anailse/ecommerce/AnailseEcommerceApplication.java
@@ -0,0 +1,14 @@
+package com.anailse.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class AnailseEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnailseEcommerceApplication.class, args);
+	}
+
+}
