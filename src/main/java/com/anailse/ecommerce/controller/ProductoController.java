bed72ddb7b1ed81b61fd2a83531cca84c4f75f14
CLAROGT\brayner.navarro <brayner.navarro@claro.com.gt>
2024-08-27 16:59:57 -0600

    se agrega el crud de productos empezando por el save

--- src/main/java/com/anailse/ecommerce/controller/ProductoController.java
+++ src/main/java/com/anailse/ecommerce/controller/ProductoController.java
@@ -1,15 +1,39 @@
 package com.anailse.ecommerce.controller;
 
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.anailse.ecommerce.model.Producto;
+import com.anailse.ecommerce.model.Usuario;
+import com.anailse.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+	
+	private final Logger LOGGER= LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private ProductoService productoService;
 
 	@GetMapping("")
 	public String show () {
 		return "productos/show";
+	}
+	@GetMapping("/create")
+	public String create() {
+		return "productos/create";
+	}
+	@PostMapping("/save")
+	public String save (Producto producto) {
+		LOGGER.info("Este es el objeto producto{}",producto.toString());
+		Usuario u= new Usuario(1,null, null, null, null, null, null, null);
+		producto.setUsuario(u);
+		productoService.save(producto);
+		return "redirect:/productos";
 	}
 }
