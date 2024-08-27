d8cf96729891bb2c1661213807744511325f84dd
CLAROGT\brayner.navarro <brayner.navarro@claro.com.gt>
2024-08-27 14:44:30 -0600

    creacion de template para administrador y creación de clases model

--- src/main/java/com/anailse/ecommerce/model/Producto.java
+++ src/main/java/com/anailse/ecommerce/model/Producto.java
@@ -1,12 +1,26 @@
 package com.anailse.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "productos")
 public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
 	private String imagen;
 	private double precio;
 	private int cantidad;
+	
+	@ManyToOne
+	private Usuario usuario;
 	
 	public Producto() {
 		// TODO Auto-generated constructor stub
@@ -68,6 +82,14 @@ public class Producto {
 
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 
 	@Override
