16eac09216ebcb01e42e23aaa7c361e137248835
CLAROGT\brayner.navarro <brayner.navarro@claro.com.gt>
2024-08-29 12:44:42 -0600

    creación del homee de usuario y carga de productos

---
+++ src/main/java/com/anailse/ecommerce/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.anailse.ecommerce.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.anailse.ecommerce.model.Producto;
+import com.anailse.ecommerce.service.ProductoService;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@Autowired
+	private ProductoService productoService;
+	
+	
+	
+	@GetMapping("")
+	public String home (Model model) {
+		List <Producto> productos=productoService.findAll();
+		model.addAttribute("productos",productos);
+		return "usuario/home";
+	}
+
+}
